233702d2f1597fed2b55098907fdec9229874540
greg0910 <89106244+greg0910@users.noreply.github.com>
2022-06-05 23:53:07 -0500

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -1,53 +1,70 @@
-from tkinter import Button, Entry, Frame, Tk
+from cProfile import label
+from cgitb import text
+from tkinter import Button, Entry, Frame, Label, Scrollbar, Tk
+import tkinter
+import serial
 
+#Ventana Principal
 ventana = Tk()
 ventana.title("Proyecto_i2c")
 ventana.geometry("400x300")
 ventana.config(cursor="hand1")
-
-
-#LEER LOS DATOS
-#Panel para los datos
+#Cuadro
 plano=Frame()
-plano.place(width=200, height=300)
+plano.place(width=400, height=300)
 plano.config(bg="lightblue")
 plano.config(bd=25)
 plano.config(relief="sunken")
 
 
+#Variables
+
+
+def dat():
+    Nucleo = serial.Serial('COM10', 9600)
+    while True:
+     rawString = str(Nucleo.readline())
+     rawString = rawString.strip("b'\.n")  # Ya tengo mi valor @num# limpio
+    # Se verifica la trama
+     if (rawString.count('@') == 1) and (rawString.count('#') == 1):
+        sensor = float(rawString.strip("@#"))
+        
+  
+
+#LEER LOS DATOS
+
+scroll=Scrollbar(ventana)
+
 
 #BOTONES
 #Boton leer voltaje
-adc = Button(ventana, text="Leer el volatje",command="").place(x = 250,y = 20)
+adc = Button(ventana, text="Leer el volatje",command="Datos").place(x = 250,y = 40)
 
 
 #Botones para encender y apagar el led
-on = Button(ventana, text="Encender",command='').place(x = 250,y = 60)
-off = Button(ventana, text="Apagar",command='').place(x = 320,y = 60)
-
-
-#Botones para encender el led por segundo
-seg = Button(ventana, text="Encender",command='').place(x = 320,y = 100)
-tx2=Entry(ventana).place(x=250,y=100,width=60, height=30)
-
+on = Button(ventana, text="Encender",command='').place(x = 250,y = 80)
+off = Button(ventana, text="Apagar",command='').place(x = 320,y = 80)
 
 #Boton para las posiciones de la eeprom
-rom = Button(ventana, text="Leer la Eeprom",command='').place(x = 250,y = 140)
+rom = Button(ventana, text="Leer la Eeprom",command='').place(x = 250,y = 120)
 
 
 #Boton para leer la posicion del bloque 2
-rom2= Button(ventana, text="Leer el 2 bloque",command='').place(x = 250,y = 180)
+rom2= Button(ventana, text="Leer el 2 bloque",command='').place(x = 250,y = 160)
 
 
 #Boton para leer los datos de sensor
-sen = Button(ventana, text="Datos del sensor",command='').place(x = 250,y = 220)
+sen = Button(ventana, text="Datos del sensor",command='').place(x = 250,y = 200)
 
 
 #Entrada texto para los comando
-enter = Button(ventana, text="Enter",command='').place(x = 320,y = 260)
-tx2=Entry(ventana).place(x=250,y=260,width=60, height=30)
+enter = Button(ventana, text="Enter",command='').place(x = 320,y =240)
+tx2=Entry(ventana).place(x=250,y=240,width=60, height=30)
 
 
 ventana.mainloop()
+
+
+
 
 
